47a115bb9f8925499e75a6bb58bf5e4304adeb4c
Dao_1213 <Austindao90@gmail.com>
2025-03-06 12:09:52 -0800

    second version

--- SWE_Onymos.py
+++ SWE_Onymos.py
@@ -2,46 +2,43 @@ import random
 
 
 
-class stock:
-    def __init__(self, ticker_symbol, quantity, price, next = None):
-        self.symbol = ticker_symbol
+class Nodes:
+    def __init__(self, quantity, price, next = None):
         self.price = price
         self.quantity = quantity
         self.next = next
-        self.index = sum( ord(ticker_symbol[i]) * (10**i) for i in range(len(ticker_symbol))) % 1024
 
-    def add_leaf(self, ticker_symbol, quantity, price):
+    def add_leaf(self, quantity, price):
         print("add leaf")
-        next_element = stock(ticker_symbol, quantity, price)
+        next_element = Nodes(quantity, price)
         curr = self
         while (not curr.is_leaf()):
             curr = curr.next
         curr.next = next_element
 
-    def add_root(self, ticker_symbol, quantity, price):
+    def add_root(self, quantity, price):
         print("add leaf")
-        temp = stock(self.symbol, self.quantity, self.price, self.next)
-        self.symbol = ticker_symbol
+        temp = Nodes(self.quantity, self.price, self.next)
         self.quantity = quantity
         self.price = price
         self.next = temp
 
-    def add_bottom(self, ticker_symbol, quantity, price):
+    def add_bottom(self, quantity, price):
         curr = self
         while curr.next:
             if curr.price >= price and price >= curr.next.price:
-                next_element = stock(ticker_symbol, quantity, price, curr.next)
+                next_element = Nodes(quantity, price, curr.next)
                 curr.next = next_element
                 return
             else:
                 curr = curr.next
-        curr.add_leaf(ticker_symbol, quantity, price)
+        curr.add_leaf(quantity, price)
 
-    def add_node(self, ticker_symbol, quantity, price):
+    def add_node(self, quantity, price):
         if price >= self.price:
-            self.add_root(ticker_symbol, quantity, price)
+            self.add_root(quantity, price)
         else:
-            self.add_bottom(ticker_symbol, quantity, price)
+            self.add_bottom(quantity, price)
     
     def is_leaf(self):
         return self.next == None
@@ -51,19 +48,27 @@ class stock:
         result = []
         curr = self
         while curr:
-            result.append(f"[{curr.symbol}, Qty: {curr.quantity}, Price: ${curr.price}, Index: {curr.index}]")
+            result.append(f"[Qty: {curr.quantity}, Price: ${curr.price}]")
             curr = curr.next
         return ' -> '.join(result)
 
+class stocks:
+    def __init__(self,ticker_symbol, quantity, price):
+        self.symbol = ticker_symbol
+        self.index = sum( ord(ticker_symbol[i]) * (10**i) for i in range(len(ticker_symbol))) % 1024
+        self.node = Nodes(quantity,price)
 
-class order:
+class type:
     def __init__(self, order_type):
         self.type = order_type
         self.list = [0] * 1024
+        self.element = []
         self.count = 0
 
-list = [0] * 10
-print (list)
+
+
+
+
 
 
 
